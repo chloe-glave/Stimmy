150b44f0b16d2ecf7f20709919dd021cd9f0d031
Chloe (Desktop) <cragzu@gmail.com>
2026-01-25 17:49:54 -0800

    add button to go back

--- app/screens/TogglesScreen.tsx
+++ app/screens/TogglesScreen.tsx
@@ -1,17 +1,22 @@
 import { FC } from "react"
 import { ViewStyle } from "react-native"
+import { useNavigation } from "@react-navigation/native"
 
+import { Button } from "@/components/Button"
 import { Screen } from "@/components/Screen"
 import { Text } from "@/components/Text"
 import type { AppStackScreenProps } from "@/navigators/navigationTypes"
-// import { useNavigation } from "@react-navigation/native"
+import { useAppTheme } from "@/theme/context"
 import { $styles } from "@/theme/styles"
+import type { ThemedStyle } from "@/theme/types"
 
 interface TogglesScreenProps extends AppStackScreenProps<"Toggles"> {}
 
+// todo: first screen - list of big toggle components switch variant
 export const TogglesScreen: FC<TogglesScreenProps> = () => {
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
+  const navigation = useNavigation<AppStackScreenProps<"Toggles">["navigation"]>()
+
+  const { themed } = useAppTheme()
   return (
     <Screen
       style={$root}
@@ -19,6 +24,11 @@ export const TogglesScreen: FC<TogglesScreenProps> = () => {
       contentContainerStyle={$styles.flex1}
       safeAreaEdges={["top", "bottom", "left", "right"]}
     >
+      <Button
+        onPress={() => navigation.navigate("Demo", { screen: "DemoShowroom", params: {} })}
+        text="Go to DemoShowroom"
+        style={themed($button)}
+      />
       <Text text="toggles" />
     </Screen>
   )
@@ -27,3 +37,8 @@ export const TogglesScreen: FC<TogglesScreenProps> = () => {
 const $root: ViewStyle = {
   flex: 1,
 }
+
+const $button: ThemedStyle<ViewStyle> = ({ spacing, colors }) => ({
+  marginHorizontal: spacing.lg,
+  backgroundColor: colors.tint,
+})
